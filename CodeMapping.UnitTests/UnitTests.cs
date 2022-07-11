6bde8c3f2c2dedfdb7ae92a327c9b7d947e343ce
Damien Rush <damien.rush@gmail.com>
2022-07-11 14:41:38 +1000

    Unit test for the data source

--- CodeMapping.UnitTests/UnitTests.cs
+++ CodeMapping.UnitTests/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using CodeMapping;
 using CodeMapping.Tables;
+using CodeMappingTest.DataSources;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,15 @@ namespace CodeMappingTest
             Dictionary<string, string> mapper = CountryCodes.CountryCodesTable.GetMap(CodeType.ExternalCode, CodeType.InternalKey);
 
             Assert.AreEqual("036", mapper["au"]);
+        }
+
+        [TestMethod]
+        [CountryCodesData(CodeType.ExternalCode, CodeType.InternalKey)]
+        public void TestCountryMapping_DataSource(string input, string output)
+        {
+            Dictionary<string, string> mapper = CountryCodes.CountryCodesTable.GetMap(CodeType.ExternalCode, CodeType.InternalKey);
+
+            Assert.AreEqual(output, mapper[input]);
         }
     }
 }
