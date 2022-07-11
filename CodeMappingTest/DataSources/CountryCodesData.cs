6c4fed19d711fafd56ca6c2f1d6c4e914b913333
Damien Rush <damien.rush@gmail.com>
2022-07-11 14:35:44 +1000

    Adding unit test data source for country code mappings.

---
+++ CodeMappingTest/DataSources/CountryCodesData.cs
@@ -0,0 +1,18 @@
+﻿using CodeMapping;
+using CodeMapping.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMappingTest.DataSources
+{
+    public class CountryCodesData : GroupCodeDataSource
+    {
+        public CountryCodesData(CodeType inputType, CodeType outputType) : base(CountryCodes.CountryCodesTable.GetMap(inputType, outputType))
+        {
+            
+        }
+    }
+}
---
+++ CodeMappingTest/GroupCodeDataSource.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMappingTest
+{
+    public abstract class GroupCodeDataSource : Attribute, ITestDataSource
+    {
+        private readonly IEnumerable<object[]> data;
+        public GroupCodeDataSource(Dictionary<string, string> groupCodeTable)
+        {
+            data = groupCodeTable.Select(kvp => new object[] { kvp.Key, kvp.Value }).AsEnumerable();
+        }
+
+        public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+        {
+            return data;
+        }
+
+        public string GetDisplayName(MethodInfo methodInfo, object[] data)
+        {
+            if (data != null)
+            {
+                return string.Format(CultureInfo.CurrentCulture, "{0} ({1})", methodInfo.Name, string.Join(",", data));
+            }
+
+            return string.Empty;
+        }
+    }
+}
